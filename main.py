fa26e9bb2452f6662d5c8f1d0a13396ac0e46947
Punit Arani <punitsai36@gmail.com>
2023-10-14 11:37:12 -0700

    Create app mockup

--- main.py
+++ main.py
@@ -1,36 +1,106 @@
 """GUI entry point."""
 
-import random
 import sys
 
-from PySide6 import QtCore, QtWidgets
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import QIcon, QFont
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QVBoxLayout,
+    QHBoxLayout,
+    QPushButton,
+    QLabel,
+    QListWidget,
+    QWidget,
+)
 
 
-class MyWidget(QtWidgets.QWidget):
+class FileManager(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+
+        uploadButton = QPushButton(QIcon.fromTheme("document-open"), "Upload Image")
+        uploadButton.setIconSize(QSize(24, 24))
+
+        self.fileList = QListWidget()
+
+        layout.addWidget(uploadButton)
+        layout.addWidget(self.fileList)
+        self.setLayout(layout)
+
+
+class MainImageViewer(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+
+        label = QLabel("Main Image Viewer")
+        label.setFont(QFont("Arial", 12, QFont.Bold))
+        label.setAlignment(Qt.AlignCenter)
+
+        self.imageView = QLabel("(Selected image will be displayed here)")
+        self.imageView.setAlignment(Qt.AlignCenter)
+
+        layout.addWidget(label)
+        layout.addWidget(self.imageView)
+        self.setLayout(layout)
+
+
+class SegmentViewer(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+
+        label = QLabel("Segments Viewer")
+        label.setFont(QFont("Arial", 12, QFont.Bold))
+        label.setAlignment(Qt.AlignCenter)
+
+        self.segmentView = QLabel(
+            "(Segments of the selected image will be displayed here)"
+        )
+        self.segmentView.setAlignment(Qt.AlignCenter)
+
+        layout.addWidget(label)
+        layout.addWidget(self.segmentView)
+        self.setLayout(layout)
+
+
+class CTScanApp(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        # Set Window Title, Size, and Styling
+        self.setWindowTitle("CT Scan Analysis")
+        screen = QApplication.primaryScreen().availableGeometry()
+        self.resize(screen.width(), screen.height())
 
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+        # Main Font
+        font = QFont()
+        font.setFamily("Arial")
+        font.setPointSize(10)
+        self.setFont(font)
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        # Use the new components
+        fileManager = FileManager()
+        mainImageViewer = MainImageViewer()
+        segmentViewer = SegmentViewer()
 
-        self.button.clicked.connect(self.magic)
+        mainLayout = QHBoxLayout()
+        mainLayout.addWidget(fileManager, 1)
+        mainLayout.addWidget(mainImageViewer, 2)
+        mainLayout.addWidget(segmentViewer, 1)
 
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(mainLayout)
+        self.setCentralWidget(centralWidget)
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    app = QApplication(sys.argv)
 
-    widget = MyWidget()
-    widget.resize(1280, 720)
-    widget.show()
+    window = CTScanApp()
+    window.show()
 
     sys.exit(app.exec())
