0853808f3ee454e8598115901985213d711f02b6
Punit Arani <punitsai36@gmail.com>
2024-04-01 08:37:27 -0700

    Add EMBEDDINGS_DIR

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ from pathlib import Path
 
 PROJECT_DIR = Path(__file__).parent
 DATA_DIR = PROJECT_DIR.joinpath("data")
+EMBEDDINGS_DIR = DATA_DIR.joinpath("embeddings")
 MODELS_DIR = PROJECT_DIR.joinpath("models")
